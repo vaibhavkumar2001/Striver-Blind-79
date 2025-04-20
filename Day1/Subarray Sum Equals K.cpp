87296cce457c9735e96285a8018b22791a14a3db
Vaibhav Kumar <126766355+vaibhavkumar2001@users.noreply.github.com>
2025-04-21 00:16:32 +0530

    Subarray Sum Equals K problem

---
+++ Day1/Subarray Sum Equals K.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+
+class Solution {
+    public:
+        int subarraySum(vector<int>& nums, int k) {
+            int n = nums.size(); // size of the given array.
+            map<int, int> mpp;
+            int preSum = 0, cnt = 0;
+    
+            mpp[0] = 1; // Setting 0 in the map.
+            for (int i = 0; i < n; i++) {
+                // Add current element to prefix Sum:
+                preSum += nums[i];
+    
+                // Calculate x-k:
+                int remove = preSum - k;
+    
+                // Add the number of subarrays to be removed:
+                if (mpp.find(remove) != mpp.end()) {
+                    cnt += mpp[remove];
+                }
+    
+                // Update the count of prefix sum in the map.
+                mpp[preSum] += 1;
+            }
+            return cnt;
+        }
+    };
